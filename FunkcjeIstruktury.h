12bfdc68d42c4458036c594f6b00c097e22cd22f
Mateusz Bielski <mateo_bass@bialan.pl>
2018-09-05 21:00:11 +0200

    wykrycie systemu w FunkcjeIstruktury.h

--- FunkcjeIstruktury.h
+++ FunkcjeIstruktury.h
@@ -3,10 +3,13 @@
 #include <GL/glu.h>
 #include <gtkglmm.h>
 
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+#else
 struct POINT{
     POINT(float x, float y):x(x),y(y){};
     float x,y;
 };
+#endif
 void WyswietlWartosciMacierzy4x4(float * macierz16);
 void TransformujPikselDoPrzestrzeniSceny(int x, int y,float glebokosc,float*  pozycjaPunktu3D);
 void TransformujPikselDoPrzestrzeniSceny(int x, int y,float*  pozycjaPunktu3D);
